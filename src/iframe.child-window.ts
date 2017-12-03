4815bf898f5a5e5d99d7554ce57e4b34a7e34f75
Yenthe Oerlemans <yentheoerlemans@gmail.com>
2017-12-03 17:01:49 +0100

    unnecessary subscribes

--- src/iframe.child-window.ts
+++ src/iframe.child-window.ts
@@ -6,9 +6,6 @@ export class Iframe extends ChildWindow {
 
     public constructor(private _iframeElement: HTMLIFrameElement) {
         super(_iframeElement.contentWindow);
-        _iframeElement.contentDocument.addEventListener('DOMContentLoaded', () => console.log('loaded iframe'));
-        _iframeElement.addEventListener('DOMContentLoaded', () => console.log('loaded iframe'));
-        _iframeElement.contentWindow.document.addEventListener('DOMContentLoaded', () => console.log('loaded iframe'));
     }
 
     protected _cleanUp(): void {
