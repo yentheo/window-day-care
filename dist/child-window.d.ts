4ce89cd1c7e8a9ac595538c98d7e5fdce229e994
Travis CI User <travis@example.org>
2017-12-04 21:52:22 +0000

    [skip ci]

--- dist/child-window.d.ts
+++ dist/child-window.d.ts
@@ -1,20 +1,42 @@
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/filter';
+import { Observable } from 'rxjs/Observable';
+/** Represents an instance of child window like an iframe or a pop-up */
 export declare abstract class ChildWindow {
     protected _window: Window;
     private _message$;
     readonly message$: Observable<any>;
     private _messageListener;
     protected constructor(_window: Window);
+    /**
+     * Sends a message to the parent window
+     * @param message A simple JSON-object
+     */
     sendMessage(message: any): void;
+    /** Closes the window */
     close(): void;
-    protected abstract _cleanUp(): void;
+    /**
+     * Sets the new location of the child window. Returns a promise that is resolved when the page is loaded.
+     * @param uri the location the child window will go to
+     */
     setLocation(uri: string): Promise<void>;
+    protected abstract _cleanUp(): void;
 }
+/** Checks whether we're in an iframe at the moment. */
+export declare function isIframe(): boolean;
+/** Checks whether we're in a pop up at the moment. */
+export declare function isPopUp(): boolean;
+/** Checks whether we're in a child window at the moment. */
 export declare function isChildWindow(): boolean;
+/** Helps you communicating from inside a child window */
 export declare class ChildWindowHost {
+    private readonly _parent;
+    /** Sends a message to this child window */
     sendMessage(message: any, targetOrigin?: string): void;
 }
+/**
+ * Returns the current child window host. This method should only be called when you are in a child window.
+ * Use the {@link isChildWindow}-method to check whether this is the case.
+ */
 export declare function getChildWindowHost(): ChildWindowHost;
 export declare enum ChildWindowMessageType {
     None = 0,
--- dist/pop-up.creator.d.ts
+++ dist/pop-up.creator.d.ts
@@ -1,4 +1,5 @@
-import { ChildWindow } from "./child-window";
+import { ChildWindow } from './child-window';
+/** Options for pop-up */
 export interface PopUpOptions {
     height?: number;
     width?: number;
@@ -11,6 +12,16 @@ export interface PopUpOptions {
     minimizable?: boolean;
     resizable?: boolean;
 }
+/** Default options */
 export declare const DefaultPopUpOptions: PopUpOptions;
+/**
+ * Opens a pop-up window.
+ * @param name name of the pop-up, if this is the same as an already open pop-up, there will be no new pop-up
+ * @param popUpOptions options for the pop-up, like width
+ */
 export declare function openPopUp(name?: string, popUpOptions?: PopUpOptions): ChildWindow;
+/**
+ * Opens a tab.
+ * @param name name of the tab, if this is the same as an already open tab, there will be no new tab
+ */
 export declare function openTab(name?: string): ChildWindow;
