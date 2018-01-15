85386ae8bd9069e58ac82fc30e43cce04e66fff7
Travis CI User <travis@example.org>
2018-01-15 21:42:59 +0000

    version [ci skip]

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
-import 'core-js';
 export { inject, injectIntoBody } from './iframe.creator';
 export { openPopUp, openTab } from './pop-up.creator';
 export { ChildWindow, isChildWindow, getChildWindowHost } from './child-window';
